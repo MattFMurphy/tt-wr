464270f6cb1fd316bb7c4e7e284b364463b1fcc1
Matt <matt.murphy@wilddogdesign.co.uk>
2024-04-06 17:55:13 +0100

    feat: featured sessions

--- src/components/FeaturedSessions/elements/Session.js
+++ src/components/FeaturedSessions/elements/Session.js
@@ -1,20 +1,23 @@
 import React from "react";
 
 const Session = ({ payload }) => {
-  const {link, title, image, thumb, time} = payload
+  const { link, title, image, thumb, time } = payload;
+
+  console.log(thumb.src);
+
   return (
-    <div className="c-featured-sessions__session">
+    <div className="c-featured-sessions__session" style={{ cursor: `url(${thumb.src}), pointer` }}>
       <a className="c-featured-sessions__session__image" href={link}>
         <img src={image.src} alt={image.alt} />
       </a>
 
-      <a href={link} title={title}>
         <h2 className="c-featured-sessions__session__title">
-          {title}
+          <a href={link} title={title}>
+            {title}
+          </a>
         </h2>
-      </a>
 
-      <a href={link} className="c-featured-sessions__session__time o-text o-text--large">{time}</a>
+      <a href={link} className="c-featured-sessions__session__time text text--large">{time}</a>
     </div>
   );
 };
