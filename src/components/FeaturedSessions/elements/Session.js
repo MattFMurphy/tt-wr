bda47b214098cd470bd720654fc468ff9150c541
Matt <matt.murphy@wilddogdesign.co.uk>
2024-04-06 18:11:33 +0100

    chore

--- src/components/FeaturedSessions/elements/Session.js
+++ src/components/FeaturedSessions/elements/Session.js
@@ -6,19 +6,19 @@ const Session = ({ payload }) => {
   console.log(thumb.src);
 
   return (
-    <div className="c-featured-sessions__session" style={{ cursor: `url(${thumb.src}), pointer` }}>
-      <a className="c-featured-sessions__session__image" href={link}>
-        <img src={image.src} alt={image.alt} />
-      </a>
+    <li className="c-featured-sessions__session" style={{ cursor: `url(${thumb.src}), pointer` }}>
+      <a className="c-featured-sessions__session__wrapper" href={link} title={title}>
+        <div className="c-featured-sessions__session__image" href={link}>
+          <img src={image.src} alt={image.alt} />
+        </div>
 
         <h2 className="c-featured-sessions__session__title">
-          <a href={link} title={title}>
-            {title}
-          </a>
+          {title}
         </h2>
 
-      <a href={link} className="c-featured-sessions__session__time text text--large">{time}</a>
-    </div>
+        <div className="c-featured-sessions__session__time text text--large">{time}</div>
+      </a>
+    </li>
   );
 };
 
